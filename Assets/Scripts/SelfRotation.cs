97fde4b80d75b9528c9e501fdf7feca06bd8bf5e
ran kaitoku <ran@bodygram.com>
2023-03-09 13:36:14 +0900

    created test scene

---
+++ Assets/Scripts/SelfRotation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotation : MonoBehaviour
+{
+    [SerializeField] private float angularSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, angularSpeed);
+    }
+}
